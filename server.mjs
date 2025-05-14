4e4005a3ca8973b938310d4897456698f41f4b18
Mu Vonglao <muvonglao@gmail.com>
2025-05-14 10:30:26 +0700

    change port to 4000

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import app from "./app.mjs";
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
