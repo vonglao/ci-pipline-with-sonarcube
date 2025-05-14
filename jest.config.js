2ff8385334d87d05169647b52f89eebbd80fce34
Mu Vonglao <muvonglao@gmail.com>
2025-05-14 09:30:26 +0700

    deleted comment from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-// jest.config.js
 export default {
   testEnvironment: "node",
   transform: {},
-  testMatch: ["**/?(*.)+(test).mjs"], // 👈 เพิ่มตรงนี้ให้ Jest หาเจอ .mjs
+  testMatch: ["**/?(*.)+(test).mjs"],
 };
